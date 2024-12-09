6362dd49a70f3819685c30e91967ac1b75e01d7a
Karikatyra <148780854+Karikatyra@users.noreply.github.com>
2024-12-10 01:41:17 +0300

    Add files via upload

--- dz1/index.php
+++ dz1/index.php
@@ -1,34 +1,37 @@
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
-<title>Домашнее задание</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
 </head>
 <body>
-
-<h1>Домашнее задание</h1>
-
-<form method="post" action="">
-  <label for="start_date">Дата начала:</label>
-  <input type="date" id="start_date" name="start_date"><br><br>
-  <label for="end_date">Дата окончания:</label>
-  <input type="date" id="end_date" name="end_date"><br><br>
-  <input type="submit" value="Готово">
-</form>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $start_date = $_POST["start_date"];
-  $end_date = $_POST["end_date"];
-
-  $start_timestamp = strtotime(date("Y-m-d", strtotime($start_date)));
-  $end_timestamp = strtotime(date("Y-m-d", strtotime($end_date)));
-
-  $days = ($end_timestamp - $start_timestamp) / (60 * 60 * 24);
-  $minutes = ($end_timestamp - $start_timestamp) / (60 * 60) * 60;
-
-  echo "Количество дней между выбранными датами: " . round($days) . "; Количество минут между выбранными датами: " . round($minutes);
-  }
-?>
-
+    <form action="" method="get">
+        <? if (isset($_GET["date1"])) { ?>
+            <input type="date" name="date1" value=<? echo $_GET["date1"] ?> required>
+            <input type="date" name="date2" value=<? echo $_GET["date2"] ?> required>
+        <? } else { ?>
+            <input type="date" name="date1" required>
+            <input type="date" name="date2" required>
+        <? } ?>
+        <input type="submit">
+    </form>
+    <?php
+    if (isset($_GET["date1"])) {
+        $date1 = strtotime($_GET["date1"]);
+        $date2 = strtotime($_GET["date2"]);
+        $second = abs($date1 - $date2);
+        $year = floor($second / 31536000);
+        $day = floor($second / 86400);
+        $hour = $day * 24;
+        $minute = $hour * 60; 
+    ?>
+        <h3>Разница дат (<? echo $_GET["date1"] ?> — <? echo $_GET["date2"] ?>):</h3>
+        <h4>В годах: <? echo $year ?></h4>
+        <h4>В днях: <? echo $day ?></h4>
+        <h4>В часах: <? echo $hour ?></h4>
+        <h4>В минутах: <? echo $minute ?></h4>
+        <h4>В секундах: <? echo $second ?></h4>
+    <? } ?>
 </body>
 </html>
